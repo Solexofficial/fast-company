0dade73eeaf91a0a57bc3bf22943f0a6a9e38c4f
Mark Pokrovsky <solexofficial@yandex.ru>
2021-08-29 14:00:33 +0300

    feat: 02. Move states and methods to the App component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import Users from "./app/components/users";
 import App from "./app/App";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Users />
 		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
