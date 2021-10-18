4e1e0b5de0cb86104674a5019ac6b013176ca64c
Mark Pokrovsky <solexofficial@yandex.ru>
2021-10-18 23:05:38 +0300

    feat: add state and handleChange for all inputs

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [data, setData] = useState({ email: '', password: '' });
+
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+  };
+
   return (
     <form action="">
       <div>
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" />
+        <input type="text" id="email" name="email" value={data.email} onChange={handleChange} />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input type="password" id="password" name="password" value={data.password} onChange={handleChange} />
       </div>
+
+      {/* <div>
+        <div>
+          <label htmlFor="radio1">radio 1</label>
+          <input type="radio" id="radio1" name="radio" />
+        </div>
+        <div>
+          <label htmlFor="radio2">radio 2</label>
+          <input type="radio" id="radio2" name="radio" />
+        </div>
+      </div> */}
     </form>
   );
 };
