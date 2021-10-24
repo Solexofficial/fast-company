637682c2652317478eb5820d0d634f2fde68459b
Mark Pokrovsky <solexofficial@yandex.ru>
2021-10-24 22:22:11 +0300

    feat: update api

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -1,9 +1,9 @@
-import users from './fake.api/user.api';
-import professions from './fake.api/professions.api';
-import qualities from './fake.api/qualities.api';
+import users from "./fake.api/user.api";
+import professions from "./fake.api/professions.api";
+import qualities from "./fake.api/qualities.api";
 const API = {
-  users,
-  professions,
-  qualities
+    users,
+    professions,
+    qualities
 };
 export default API;
