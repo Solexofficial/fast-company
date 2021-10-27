05d20911292c32dce0b40cc517010c867e2249c8
Mark Pokrovsky <solexofficial@yandex.ru>
2021-10-27 23:20:47 +0300

    feat: add formatDate for published comments

---
+++ src/app/utils/formatDate.js
@@ -0,0 +1,55 @@
+import declOfNum from './declOfNum';
+
+export default function formatDate(value) {
+  if (typeof value === 'string') {
+    value = Number(value);
+  }
+  const currentDateTime = Date.now();
+  const postCreatedTime = Number(value);
+  const diffTime = Math.abs(currentDateTime - postCreatedTime);
+
+  const year = new Date(value).getFullYear();
+  const month = new Date(value).getMonth();
+  const day = new Date(value).getDate();
+  const hours = new Date(value).getHours();
+  const min = new Date(value).getMinutes();
+
+  const months = [
+    'Января',
+    'Февраля',
+    'Марта',
+    'Апреля',
+    'Мая',
+    'Июня',
+    'Июля',
+    'Августа',
+    'Сентября',
+    'Октября',
+    'Ноября',
+    'Декабря'
+  ];
+
+  const checkLeapYear = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+  };
+
+  const oneMinutesAgo = 60000;
+  const thirtyMinutesAgo = oneMinutesAgo * 30;
+  const lessOneDay = oneMinutesAgo * 60 * 24;
+  const lessCurrentYear = lessOneDay * (checkLeapYear(year) ? 366 : 365);
+
+  if (diffTime <= oneMinutesAgo) {
+    return 'только что';
+  } else if (diffTime > oneMinutesAgo && diffTime <= thirtyMinutesAgo) {
+    const minutes = Math.floor(diffTime / 60000);
+    return `${minutes} ${declOfNum(minutes, ['минута', 'минуты', 'минут'])} назад`;
+  } else if (diffTime > thirtyMinutesAgo && diffTime <= lessOneDay) {
+    return `сегодня в 0${hours}:0${min}`;
+  } else if (diffTime > lessOneDay && diffTime <= lessCurrentYear) {
+    return `${day} ${months[month]} в 0${hours}:0${min}`;
+  } else if (diffTime > lessCurrentYear) {
+    return `${day} ${months[month]} ${year} года в 0${hours}:0${min}`;
+  } else {
+    return `Этот комментарий оставило НЛО из будущего ¯\\_(ツ)_/¯`;
+  }
+}
