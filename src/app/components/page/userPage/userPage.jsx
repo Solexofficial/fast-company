7c07533c461e6a9bd7f39bf343b25944ffe9a1c4
Mark Pokrovsky <solexofficial@yandex.ru>
2021-10-30 15:11:00 +0300

    refactor: userCard

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -1,35 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import API from '../../../api';
-import { useHistory } from 'react-router';
 import MeetingsCard from '../../ui/cards/meetingsCard';
 import UserCard from '../../ui/cards/userCard';
 import QualitiesCard from '../../ui/cards/qualitiesCard';
 import UserCommentsContainer from '../../ui/userCommentsContainer';
 
 const UserPage = ({ userId }) => {
-  const history = useHistory();
   const [user, setUser] = useState();
 
   useEffect(() => {
     API.users.getById(userId).then((data) => setUser(data));
   }, []);
 
-  const handleClick = () => {
-    history.push(`/users/${userId}/edit`);
-  };
-
   if (user) {
     return (
       <div className="container">
         <div className="row gutters-sm">
           <div className="col-md-4 mb-3">
-            <UserCard
-              name={user.name}
-              profession={user.profession}
-              rate={user.rate}
-              onClick={handleClick}
-            />
+            <UserCard user={user} />
             <QualitiesCard qualities={user.qualities} />
             <MeetingsCard completedMeetings={user.completedMeetings} />
           </div>
--- src/app/components/ui/cards/userCard.jsx
+++ src/app/components/ui/cards/userCard.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router';
 
-const UserCard = ({ name, profession, rate, onClick }) => {
+const UserCard = ({ user }) => {
+  const history = useHistory();
+  const handleClick = () => {
+    history.push(history.location.pathname + '/edit');
+  };
+
   return (
     <div className="card mb-3">
       <div className="card-body">
         <button
           className="position-absolute top-0 end-0 btn btn-light btn-sm"
           style={{ zIndex: 2 }}
-          onClick={onClick}>
+          onClick={handleClick}>
           <i className="bi bi-gear"></i>
         </button>
         <div className="d-flex flex-column align-items-center text-center position-relative">
@@ -20,12 +26,12 @@ const UserCard = ({ name, profession, rate, onClick }) => {
             width="150"
           />
           <div className="mt-3">
-            <h4>{name}</h4>
-            <p className="text-secondary mb-1">{profession.name}</p>
+            <h4>{user.name}</h4>
+            <p className="text-secondary mb-1">{user.profession.name}</p>
             <div className="text-muted">
               <i className="bi bi-caret-down-fill text-primary" role="button"></i>
               <i className="bi bi-caret-up text-secondary" role="button"></i>
-              <span className="ms-2">{rate}</span>
+              <span className="ms-2">{user.rate}</span>
             </div>
           </div>
         </div>
@@ -35,10 +41,7 @@ const UserCard = ({ name, profession, rate, onClick }) => {
 };
 
 UserCard.propTypes = {
-  name: PropTypes.string,
-  profession: PropTypes.object,
-  rate: PropTypes.number,
-  onClick: PropTypes.func
+  user: PropTypes.object.isRequired
 };
 
 export default UserCard;
