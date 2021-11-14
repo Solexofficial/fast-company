7b8b1fdf5446d00d542f80b9666a92e984247db0
Mark Pokrovsky <solexofficial@yandex.ru>
2021-11-14 16:03:46 +0300

    feat: add http service, usersProvider

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -3,17 +3,23 @@ import { useParams } from 'react-router';
 import UsersListPage from '../components/page/usersListPage';
 import UserPage from '../components/page/userPage';
 import UserEditPage from '../components/page/userEditPage/userEditPage';
-
+import UserProvider from '../hooks/useUsers';
 const Users = () => {
   const params = useParams();
   const { userId, edit } = params;
 
-  return userId && edit ? (
-    <UserEditPage />
-  ) : userId ? (
-    <UserPage userId={userId} />
-  ) : (
-    <UsersListPage />
+  return (
+    <>
+      <UserProvider>
+        {userId && edit ? (
+          <UserEditPage />
+        ) : userId ? (
+          <UserPage userId={userId} />
+        ) : (
+          <UsersListPage />
+        )}
+      </UserProvider>
+    </>
   );
 };
 
