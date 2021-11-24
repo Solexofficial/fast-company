8b46002dff923956a8553f0d4abee0d3ecca342e
Mark Pokrovsky <solexofficial@yandex.ru>
2021-11-24 23:16:27 +0300

    feat: add axios interceptors, if FireBase true

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,8 +1,23 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import config from '../config.json';
+import configFile from '../config.json';
 
-axios.defaults.baseURL = config.apiEndPoint;
+axios.defaults.baseURL = configFile.apiEndPoint;
+
+axios.interceptors.request.use(
+  function (config) {
+    if (configFile.isFireBase) {
+      const containSlash = /\/$/gi.test(config.url);
+      config.url = (containSlash ? config.url.slice(0, -1) : config.url) + '.json';
+    }
+    console.log('config', config.url);
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
 axios.interceptors.response.use(
   (res) => res,
   function (error) {
