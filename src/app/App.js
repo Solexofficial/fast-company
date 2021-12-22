b60403ea856e1a2b6af786be30096bc7b241814d
Mark Pokrovsky <solexofficial@yandex.ru>
2021-12-22 21:44:12 +0300

    feat: add protectedRoute

--- src/app/App.js
+++ src/app/App.js
@@ -9,6 +9,7 @@ import NavBar from './components/ui/navBar';
 import { ProfessionProvider } from './hooks/useProfession';
 import { QualitiesProvider } from './hooks/useQuality';
 import AuthProvider from './hooks/useAuth';
+import ProtectedRoute from './components/common/protectedRoute';
 
 const App = () => {
   return (
@@ -18,7 +19,7 @@ const App = () => {
         <QualitiesProvider>
           <ProfessionProvider>
             <Switch>
-              <Route path="/users/:userId?/:edit?" component={Users} />
+              <ProtectedRoute path="/users/:userId?/:edit?" component={Users} />
               <Route path="/login/:type?" component={Login} />
               <Route exact path="/" component={Main} />
               <Redirect to="/" />
