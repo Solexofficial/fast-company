1431cab1de8b2bbff5133bd1d5675a43f3c9abb1
Mark Pokrovsky <solexofficial@yandex.ru>
2022-01-25 22:22:18 +0300

    feat: add server config, enviroment, middlewars

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const chalk = require('chalk');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = config.get('port') ?? 8080;
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('Production');
+} else {
+  console.log('####: Development');
+}
+
+app.listen(PORT, () => console.log(chalk.green(`Server has been started on port ${PORT}...`)));
