7271abe70b90f4a7ba79deee5bcc6f09c2cd4e77
Mark Pokrovsky <solexofficial@yandex.ru>
2022-01-30 23:40:19 +0300

    feat: add refreshToken

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -104,6 +104,32 @@ router.post('/signInWithPassword', [
   },
 ]);
 
-router.post('/token', async (req, res) => {});
+function isTokenInvalid(data, dbToken) {
+  return !data || !dbToken || data._id !== dbToken?.user?.toString();
+}
+
+router.post('/token', async (req, res) => {
+  try {
+    const { refresh_token: refreshToken } = req.body;
+    const data = tokenService.validateRefresh(refreshToken);
+    const dbToken = await tokenService.findToken(refreshToken);
+
+    if (isTokenInvalid(data, dbToken)) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const tokens = await tokenService.generate({
+      _id: data._id,
+    });
+
+    await tokenService.save(data._id, tokens.refreshToken);
+
+    res.status(200).send({ ...tokens, userId: data._id });
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже',
+    });
+  }
+});
 
 module.exports = router;
