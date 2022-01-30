5ba16cbb62edaa2d2e3cc9f8327c278323bec5f5
Mark Pokrovsky <solexofficial@yandex.ru>
2022-01-30 23:01:59 +0300

    feat: add req validation

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,40 +1,55 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const { generateUserData } = require('../utils/helpers');
 const router = express.Router({ mergeParams: true });
 const tokenService = require('../services/token.service');
 
-router.post('/signUp', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post('/signUp', [
+  check('email', 'Некорректный email').isEmail(),
+  check('password', 'Минимальная длина пароля 8 символов').isLength({ min: 8 }),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: {
+            message: 'INVALID DATA',
+            code: 400,
+          },
+        });
+      }
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({
-        error: {
-          message: 'EMAIL_EXISTS',
-          code: 400,
-        },
+      const { email, password } = req.body;
+
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({
+          error: {
+            message: 'EMAIL_EXISTS',
+            code: 400,
+          },
+        });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const newUser = await User.create({
+        ...generateUserData(),
+        ...req.body,
+        password: hashedPassword,
+      });
+
+      const tokens = tokenService.generate({ _id: newUser._id });
+      await tokenService.save(newUser._id, tokens.refreshToken);
+      res.status(201).send({ ...tokens, userId: newUser._id });
+    } catch (error) {
+      res.status(500).json({
+        message: 'На сервере произошла ошибка. Попробуйте позже',
       });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const newUser = await User.create({
-      ...generateUserData(),
-      ...req.body,
-      password: hashedPassword,
-    });
-
-    const tokens = tokenService.generate({ _id: newUser._id });
-    await tokenService.save(newUser._id, tokens.refreshToken);
-    res.status(201).send({ ...tokens, userId: newUser._id });
-  } catch (error) {
-    res.status(500).json({
-      message: 'На сервере произошла ошибка. Попробуйте позже',
-    });
-  }
-});
+  },
+]);
 
 router.post('/signInWithPassword', async (req, res) => {});
 
