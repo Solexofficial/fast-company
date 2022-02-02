06ba366f61756fc7f92b3b550e64f16e9fc9de1d
Mark Pokrovsky <solexofficial@yandex.ru>
2022-02-02 23:23:36 +0300

    feat: add production server mode

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const config = require('config');
+const path = require('path');
 const chalk = require('chalk');
 const initDatabase = require('./startUp/initDatabase');
 const routes = require('./routes');
@@ -12,6 +13,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use('/api', routes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, '..', 'client', 'build')));
+
+  const indexPath = path.join(__dirname, '..', 'client', 'build', 'index.html');
+
+  app.get('*', (req, res) => {
+    res.sendFile('index.html', { indexPath });
+  });
+}
 
 const PORT = config.get('port') ?? 8080;
 
